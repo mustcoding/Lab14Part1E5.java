82db6a4dac2700b2019530a9a120e5e6ea43be0a
mustcoding <mahmormust@gmail.com>
2023-07-05 08:28:16 +0800

    lab14_Part1_Exercise5

---
+++ src/Controller/calculator.java
@@ -0,0 +1,28 @@
+package Controller;
+
+public class calculator {
+
+		
+		/**
+		 * A method that will get the summation from two numbers
+		 * @param number1
+		 * @param number2
+		 * @param number3
+		 * @return summation of the numbers
+		 * @return multiplication of the numbers
+		 */
+		public int numberSum(int number1, int number2, int number3) {
+			
+			return number1 + number2 + number3;
+			
+		}
+		
+		public int numberMultiplication (int number1, int number2, int number3) {
+			
+			return number1 * number2 * number3;
+			
+		}
+
+	
+
+}
